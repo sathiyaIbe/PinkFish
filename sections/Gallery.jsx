b93a37b56f1b63df7d6bef5858159b3c28b2f733
sathiyaibe <sathiyaibd@gmail.com>
2023-10-04 13:24:45 +0530

    added

--- sections/Gallery.jsx
+++ sections/Gallery.jsx
@@ -56,8 +56,12 @@ return(
     <div id="container" onMouseMove={()=>mouseMove()}  onMouseDown={(e)=>mouseDown(e)} onMouseUp={(e)=>mouseUp(e)}  className="flex gallery_main_container gap-4 hidden md:block">
       <div className="flex gap-8 ">
       <div className="flex flex-col gap-8 ">
-        <img  onMouseOver={(e)=>mouseDowns(".image_1")} onMouseLeave={(e)=>mouseOut(".image_1")}   draggable="false" className="min-w-[400px] h-[260px] image_1" src="/Gallery/gallery_1.png" />
-        <img  onMouseOver={(e)=>mouseDowns(".image_2")} onMouseLeave={(e)=>mouseOut(".image_2")} draggable="false" className="w-[400px] h-[400px] image_2" src="/Gallery/gallery_2.png" />
+        <div className="image_11">
+        <img draggable="false" className="min-w-[400px] h-[260px] image_1" src="/Gallery/gallery_1.png" />
+        </div>
+        <div className="image_11">
+        <img draggable="false" className="w-[400px] h-[400px] image_2" src="/Gallery/gallery_2.png" />
+        </div>
       </div>
       <div className="flex flex-col gap-8  ">
         <div className="flex flex-col gap-8  gallery_container text-white p-4 ">
@@ -68,48 +72,61 @@ return(
          
             </div>
         </div>
-        <img  onMouseOver={(e)=>mouseDowns(".image_3")} onMouseLeave={(e)=>mouseOut(".image_3")} draggable="false" className="w-[440px] h-[354px] image_3" src="/Gallery/gallery_3.png" />
+       
+        <div className="image_11">
+        <img  draggable="false" className="w-[440px] h-[354px] image_3" src="/Gallery/gallery_3.png" />
       </div>
-      <div className="flex flex-col gap-8 ">
-        <img  onMouseOver={(e)=>mouseDowns(".image_4")} onMouseLeave={(e)=>mouseOut(".image_4")} draggable="false" className="min-w-[400px] h-[690px] image_4" src="/Gallery/gallery_4.png" />
       </div>
+      {/* <div className="flex flex-col gap-8 image_11 ">
+        <img  draggable="false" className="min-w-[400px] h-[690px] image_4" src="/Gallery/gallery_4.png" />
+      </div> */}
     <div className="flex flex-col gap-8 ">
         <div className="flex gap-8">
-        <img  onMouseOver={(e)=>mouseDowns(".image_5")} onMouseLeave={(e)=>mouseOut(".image_5")} draggable="false" className="image_5" src="/Gallery/gallery_5.png" />
-        <img  onMouseOver={(e)=>mouseDowns(".image_6")} onMouseLeave={(e)=>mouseOut(".image_6")} draggable="false" className="image_6" src="/Gallery/gallery_6.png" />
+        <div className="image_11">
+        <img   draggable="false"  src="/Gallery/gallery_5.png" />
         </div>
-        <div className="w-[840px] h-[308px]">
-
-        <img  onMouseOver={(e)=>mouseDowns(".image_7")} onMouseLeave={(e)=>mouseOut(".image_7")} draggable="false" className="object-contain image_7" src="/Gallery/gallery_7.png"/>
+        <div className="image_11">
+        <img draggable="false" src="/Gallery/gallery_6.png" />
+        </div>
+        </div>
+        <div className="w-[840px] h-[308px] ">
+        <div className="image_11">
+        <img draggable="false" className="object-contain " src="/Gallery/gallery_7.png"/>
+        </div>
         </div>
       </div> 
       <div className="flex flex-col gap-8 ">
+      <div className="image_11">
       
-        <img  onMouseOver={(e)=>mouseDowns(".image_8")} onMouseLeave={(e)=>mouseOut(".image_8")} className="image_8" draggable="false" src="/Gallery/gallery_8.png" />
-     
-        <div className="w-[400px] h-[400px]">
+        <img   className="image_8" draggable="false" src="/Gallery/gallery_8.png" />
+        </div>
+        <div className="w-[400px] h-[400px] image_11">
 
-        <img  onMouseOver={(e)=>mouseDowns(".image_9")} onMouseLeave={(e)=>mouseOut(".image_9")} draggable="false" className="image_9 object-contain" src="/Gallery/gallery_9.png"/>
+        <img   draggable="false" className="image_9 object-contain" src="/Gallery/gallery_9.png"/>
         </div>
       </div> 
       <div className="flex flex-col gap-8 ">
       
     
    
-      <div className="w-[400px]">
+      <div className="w-[400px] image_11">
 
-      <img  onMouseOver={(e)=>mouseDowns(".image_10")} onMouseLeave={(e)=>mouseOut(".image_10")} draggable="false" className=" h-[690px] image_10" src="/Gallery/gallery_10.png"/>
+      <img  draggable="false" className=" h-[690px] image_10" src="/Gallery/gallery_10.png"/>
       </div>
     </div> 
     <div className="flex flex-col gap-8 ">
        
-        <div className="w-[840px]">
+        <div className="w-[840px] image_11">
 
-        <img  onMouseOver={(e)=>mouseDowns(".image_111")} onMouseLeave={(e)=>mouseOut(".image_111")} draggable="false" className="object-contain image_111" src="/Gallery/gallery_11.png"/>
+        <img draggable="false" className="object-contain " src="/Gallery/gallery_11.png"/>
         </div>
         <div className="flex gap-8  ">
-        <img  onMouseOver={(e)=>mouseDowns(".image_12")} onMouseLeave={(e)=>mouseOut(".image_12")} draggable="false" className="image_12" src="/Gallery/gallery_12.png" />
-        <img  onMouseOver={(e)=>mouseDowns(".image_13")} onMouseLeave={(e)=>mouseOut(".image_13")} draggable="false" className="image_13" src="/Gallery/gallery_13.png" />
+        <div className="image_11">
+        <img draggable="false" className="image_12" src="/Gallery/gallery_12.png" />
+        </div>
+        <div className="image_11">
+        <img   draggable="false" className="image_13" src="/Gallery/gallery_13.png" />
+        </div>
         </div>
       </div> 
  
@@ -126,24 +143,33 @@ return(
         </div>
         <div className="flex gap-4 mx-2">
           <div className="flex flex-col gap-4 basis-1/2">
-          <img  onMouseOver={(e)=>mouseDowns(".image_11")} onMouseLeave={(e)=>mouseOut(".image_11")} className=" w-full h-full" src="/Gallery/gallery_1_mob.png" />
-          <img  onMouseOver={(e)=>mouseDowns(".image_11")} onMouseLeave={(e)=>mouseOut(".image_11")} className=" w-full h-full" src="/Gallery/gallery_3_mob.png" />
-
+          <div className="image_11">
+          <img className=" w-full h-full image_14" src="/Gallery/gallery_1_mob.png" />
+          </div>
+          <div className="image_11">
+          <img className=" w-full h-full" src="/Gallery/gallery_3_mob.png" />
+          </div>
           </div>
           <div className="flex flex-col gap-4 basis-1/2">
-          <img  onMouseOver={(e)=>mouseDowns(".image_11")} onMouseLeave={(e)=>mouseOut(".image_11")} className="  w-full h-full" src="/Gallery/gallery_2_mob.png" />
-          <img  onMouseOver={(e)=>mouseDowns(".image_11")} onMouseLeave={(e)=>mouseOut(".image_11")} className="  w-full h-full " src="/Gallery/gallery_4_mob.png" />
-
+          <div className="image_11">
+          <img   className="  w-full h-full" src="/Gallery/gallery_2_mob.png" />
+          </div>
+          <div className="image_11">
+          <img  className="  w-full h-full " src="/Gallery/gallery_4_mob.png" />
+          </div>
           </div>
          
         </div>
-        <img  onMouseOver={(e)=>mouseDowns(".image_11")} onMouseLeave={(e)=>mouseOut(".image_11")} className=" w-full h-full mx-2 my-4 pr-3  mt-4 mb-4" src="/Gallery/gallery_5_mob.png"/>
-        <div className="flex gap-4 mx-2 w-full ">
-          <div className="basis-1/2">
-        <img  onMouseOver={(e)=>mouseDowns(".image_11")} onMouseLeave={(e)=>mouseOut(".image_11")} className="object-cover w-full h-full rounded" src="/Gallery/gallery_6_mob.png" />
+        
+        <div className="image_11">
+        <img  className=" w-full h-full mx-2 my-4 pr-3  mt-4 mb-4" src="/Gallery/gallery_5_mob.png"/>
         </div>
-        <div className="basis-1/2 mr-3 ">
-          <img  onMouseOver={(e)=>mouseDowns(".image_11")} onMouseLeave={(e)=>mouseOut(".image_11")} className="w-full h-full " src="/Gallery/gallery_7_mob.png" />
+        <div className="flex gap-4 mx-2 w-full ">
+          <div className="basis-1/2 image_11">
+        <img className="object-cover w-full h-full rounded" src="/Gallery/gallery_6_mob.png" />
+        </div>
+        <div className="basis-1/2 mr-3 image_11 ">
+          <img className="w-full h-full " src="/Gallery/gallery_7_mob.png" />
         </div>
         </div>
     </div>
